9db9127715705f8eb302e54f3d8ca63ad2203755
Nathan Morse <nathanzmorse@gmail.com>
2017-02-15 20:55:15 -0500

    working correctly!

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject guitar-ipsum "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :description "Lorem Ipsum, except with guitars!"
+  :url "https://guitar-ipsum.herokuapp.com/"
+  :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  				[compojure "1.4.0"]
+  				[ring/ring-jetty-adapter "1.4.0"]
+  				[environ "1.0.0"]]
   :main ^:skip-aot guitar-ipsum.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:production {:env {:production true}}})
--- src/guitar_ipsum/core.clj
+++ src/guitar_ipsum/core.clj
@@ -1,7 +1,22 @@
 (ns guitar-ipsum.core
-  (:gen-class))
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [compojure.route :as route]
+            [clojure.java.io :as io]
+            [ring.adapter.jetty :as jetty]
+            [environ.core :refer [env]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn splash []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello from Heroku"})
+
+(defroutes app
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
