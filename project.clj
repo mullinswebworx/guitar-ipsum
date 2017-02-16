df809e4437f67b6dd5cc0b91d21bab83be5d3060
Nathan Morse <nathanzmorse@gmail.com>
2017-02-15 22:13:51 -0500

    curious to see if this will work...

--- project.clj
+++ project.clj
@@ -10,5 +10,5 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
+  :uberjar-name "guitar-ipsum-standalone.jar"
   :profiles {:production {:env {:production true}}})
