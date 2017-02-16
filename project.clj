4217e8c888ecdcd1c4ccb0f1664d25a75aa8aea1
Nathan Morse <nathanzmorse@gmail.com>
2017-02-15 21:47:40 -0500

    hopefully will still work...

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject guitar-ipsum "1.0.0-SNAPSHOT"
-  :description "Demo Clojure web app"
-  :url "http://clojure-getting-started.herokuapp.com"
+  :description "Lorem Ipsum... except with guitars!"
+  :url "https://guitar-ipsum.herokuapp.com/"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -10,5 +10,5 @@
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
+  :uberjar-name "guitar-ipsum.jar"
   :profiles {:production {:env {:production true}}})
